4f4e44fe93b1af6a6ae98173ef95a3e951841264
Jessica Erazo <jessica.erazo@freshrealm.co>
2021-04-07 15:31:59 -0700

    clean up

--- js/barchart.js
+++ js/barchart.js
@@ -1,98 +1,93 @@
-// D3 barchart example with json data
-
-const sample = [
-  {
-    language: 'Rust',
-    value: 78.9,
-    color: '#000000'
-  },
-  {
-    language: 'Kotlin',
-    value: 75.1,
-    color: '#00a2ee'
-  },
-  {
-    language: 'Python',
-    value: 68.0,
-    color: '#fbcb39'
-  },
-  {
-    language: 'TypeScript',
-    value: 67.0,
-    color: '#007bc8'
-  },
-  {
-    language: 'Go',
-    value: 65.6,
-    color: '#65cedb'
-  },
-  {
-    language: 'Swift',
-    value: 65.1,
-    color: '#ff6e52'
-  },
-  {
-    language: 'JavaScript',
-    value: 61.9,
-    color: '#f9de3f'
-  },
-  {
-    language: 'C#',
-    value: 60.4,
-    color: '#5d2f8e'
-  },
-  {
-    language: 'F#',
-    value: 59.6,
-    color: '#008fc9'
-  },
-  {
-    language: 'Clojure',
-    value: 59.6,
-    color: '#507dca'
-  }
-];
+// d3 barchart using json file for the data
 
 const margin = 80;
 const width = 1000 - 2 * margin;
 const height = 800 - 2 * margin;
 
-const svg = d3.select("#chart-area").append("svg")
-  .attr("width", width)
-  .attr("height", 1000)
+d3.json("/data/popular-lang-2018.json").then(data => {
+  data.forEach(d => {
+    d.value = Number(d.value)
+  });
 
-const chart = svg.append('g')
-  .attr('transform', `translate(${margin}, ${margin})`);
+  const svg = d3.select("#barchart-area").append("svg")
+    .attr("width", width)
+    .attr("height", 1000)
 
-const xScale = d3.scaleBand()
-  .range([0, width])
-  .domain(sample.map((s) => s.language))
-  .padding(0.4)
+  const chart = svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
 
-const yScale = d3.scaleLinear()
-  .range([height, 0])
-  .domain([0, 100]);
+  const xScale = d3.scaleBand()
+    .range([0, 760])
+    .domain(data.map((s) => s.language))
+    .padding(0.3)
 
-const makeYLines = () => d3.axisLeft()
-  .scale(yScale)
+  const yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, 100]);
 
-chart.append('g')
-  .attr('transform', `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale));
+  chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
 
-chart.append('g')
-  .call(d3.axisLeft(yScale));
+  chart.append('g')
+    .call(d3.axisLeft(yScale));
+
+  chart.append('g')
+    .attr('class', 'grid')
+    .call(d3.axisLeft()
+      .scale(yScale)
+      .tickSize(-width, 0, 0)
+      .tickFormat(''))
+
+  const barGroups = chart.selectAll()
+    .data(data)
+    .enter()
+    .append('g')
+
+  barGroups
+    .append('rect')
+    .attr('x', (g) => xScale(g.language))
+    .attr('y', (g) => yScale(g.value))
+    .attr('height', (g) => height - yScale(g.value))
+    .attr('width', xScale.bandwidth())
+    .attr('fill', (g) => g.color)
+
+  barGroups
+    .append('text')
+    .attr('class', 'value')
+    .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+    .attr('y', (a) => yScale(a.value) + 30)
+    .attr('text-anchor', 'middle')
+    .text((a) => `${a.value}%`)
+
+  svg
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', -(height / 2) - margin)
+    .attr('y', margin / 2.4)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Meter (%)')
+
+  svg.append('text')
+    .attr('class', 'label')
+    .attr('x', width / 2 + margin)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'middle')
+    .text('Languages')
+
+  svg.append('text')
+    .attr('class', 'title')
+    .attr('x', width / 2 + margin)
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text('Most loved programming languages in 2018')
 
 
-const barGroups = chart.selectAll()
-  .data(sample)
-  .enter()
-  .append('g')
 
-barGroups
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', (g) => xScale(g.language))
-  .attr('y', (g) => yScale(g.value))
-  .attr('height', (g) => height - yScale(g.value))
-  .attr('width', xScale.bandwidth())
+
+
+}).catch(e => {
+  console.log(e);
+})
+
